2c127a54304abc28a6b9b770aacc4d28bf3dcfaa
robert611 <cyrkiel86@interia.pl>
2020-04-21 17:33:26 +0200

    Fixed bug in fixtures, before league owner was not add to the database as one of the league players

--- src/DataFixtures/UserSeasonPlayersFixtures.php
+++ src/DataFixtures/UserSeasonPlayersFixtures.php
@@ -40,15 +40,19 @@ class UserSeasonPlayersFixtures extends Fixture implements DependentFixtureInter
     public function getPlayers()
     {
         return [
+            /* There should be an owner of the league also added as a player */
+            ['season_id' => 1, 'user_id' => 1, 'driver_id' => 2],
             ['season_id' => 1, 'user_id' => 5, 'driver_id' => 1],
             ['season_id' => 1, 'user_id' => 2, 'driver_id' => 10],
             ['season_id' => 1, 'user_id' => 3, 'driver_id' => 15],
             ['season_id' => 1, 'user_id' => 4, 'driver_id' => 20],
 
+            ['season_id' => 2, 'user_id' => 2, 'driver_id' => 1],
             ['season_id' => 2, 'user_id' => 1, 'driver_id' => 2],
             ['season_id' => 2, 'user_id' => 4, 'driver_id' => 4],
             ['season_id' => 2, 'user_id' => 3, 'driver_id' => 6],
 
+            ['season_id' => 3, 'user_id' => 3, 'driver_id' => 1],
             ['season_id' => 3, 'user_id' => 5, 'driver_id' => 19],
             ['season_id' => 3, 'user_id' => 4, 'driver_id' => 9]
         ];
