c9efadd8f714e7492e19ea5e8a12ad14b880f63d
robert611 <cyrkiel86@interia.pl>
2020-04-22 13:37:20 +0200

    Fixed bug, with lack of league owners as one of the league players in fixtures

--- src/DataFixtures/UserSeasonQualificationsFixtures.php
+++ src/DataFixtures/UserSeasonQualificationsFixtures.php
@@ -35,54 +35,64 @@ class UserSeasonQualificationsFixtures extends Fixture implements DependentFixtu
     private function getQualifications()
     {
         return [
-            // Season id = 1, players = 4
+            // Season id = 1, players = 5
             ['player_id' => '1', 'race_id' => '1', 'position' => 1],
             ['player_id' => '2', 'race_id' => '1', 'position' => 2],
             ['player_id' => '3', 'race_id' => '1', 'position' => 3],
             ['player_id' => '4', 'race_id' => '1', 'position' => 4],
+            ['player_id' => '5', 'race_id' => '1', 'position' => 5],
 
 
             ['player_id' => '1', 'race_id' => '2', 'position' => 1],
             ['player_id' => '2', 'race_id' => '2', 'position' => 2],
             ['player_id' => '3', 'race_id' => '2', 'position' => 3],
             ['player_id' => '4', 'race_id' => '2', 'position' => 4],
+            ['player_id' => '5', 'race_id' => '2', 'position' => 5],
 
             ['player_id' => '1', 'race_id' => '3', 'position' => 1],
             ['player_id' => '2', 'race_id' => '3', 'position' => 2],
             ['player_id' => '3', 'race_id' => '3', 'position' => 3],
             ['player_id' => '4', 'race_id' => '3', 'position' => 4],
+            ['player_id' => '5', 'race_id' => '3', 'position' => 5],
 
             ['player_id' => '1', 'race_id' => '4', 'position' => 1],
             ['player_id' => '2', 'race_id' => '4', 'position' => 2],
             ['player_id' => '3', 'race_id' => '4', 'position' => 3],
             ['player_id' => '4', 'race_id' => '4', 'position' => 4],
+            ['player_id' => '5', 'race_id' => '4', 'position' => 5],
 
             ['player_id' => '1', 'race_id' => '5', 'position' => 1],
             ['player_id' => '2', 'race_id' => '5', 'position' => 2],
             ['player_id' => '3', 'race_id' => '5', 'position' => 3],
             ['player_id' => '4', 'race_id' => '5', 'position' => 4],
+            ['player_id' => '5', 'race_id' => '5', 'position' => 5],
 
             ['player_id' => '1', 'race_id' => '6', 'position' => 1],
             ['player_id' => '2', 'race_id' => '6', 'position' => 2],
             ['player_id' => '3', 'race_id' => '6', 'position' => 3],
             ['player_id' => '4', 'race_id' => '6', 'position' => 4],
+            ['player_id' => '5', 'race_id' => '6', 'position' => 5],
 
-            // Season id = 2, players = 3, races = 4
-            ['player_id' => '5', 'race_id' => '1', 'position' => 1],
-            ['player_id' => '6', 'race_id' => '1', 'position' => 2],
-            ['player_id' => '7', 'race_id' => '1', 'position' => 3],
+            // Season id = 2, players = 4, races = 4
+            ['player_id' => '6', 'race_id' => '7', 'position' => 1],
+            ['player_id' => '7', 'race_id' => '7', 'position' => 2],
+            ['player_id' => '8', 'race_id' => '7', 'position' => 3],
+            ['player_id' => '9', 'race_id' => '7', 'position' => 4],
 
-            ['player_id' => '5', 'race_id' => '2', 'position' => 1],
-            ['player_id' => '6', 'race_id' => '2', 'position' => 2],
-            ['player_id' => '7', 'race_id' => '2', 'position' => 3],
+            ['player_id' => '6', 'race_id' => '8', 'position' => 1],
+            ['player_id' => '7', 'race_id' => '8', 'position' => 2],
+            ['player_id' => '8', 'race_id' => '8', 'position' => 3],
+            ['player_id' => '9', 'race_id' => '8', 'position' => 4],
 
-            ['player_id' => '5', 'race_id' => '3', 'position' => 1],
-            ['player_id' => '6', 'race_id' => '3', 'position' => 2],
-            ['player_id' => '7', 'race_id' => '3', 'position' => 3],
+            ['player_id' => '6', 'race_id' => '9', 'position' => 1],
+            ['player_id' => '7', 'race_id' => '9', 'position' => 2],
+            ['player_id' => '8', 'race_id' => '9', 'position' => 3],
+            ['player_id' => '9', 'race_id' => '9', 'position' => 4],
 
-            ['player_id' => '5', 'race_id' => '4', 'position' => 1],
-            ['player_id' => '6', 'race_id' => '4', 'position' => 2],
-            ['player_id' => '7', 'race_id' => '4', 'position' => 3],
+            ['player_id' => '6', 'race_id' => '10', 'position' => 1],
+            ['player_id' => '7', 'race_id' => '10', 'position' => 2],
+            ['player_id' => '8', 'race_id' => '10', 'position' => 3],
+            ['player_id' => '9', 'race_id' => '10', 'position' => 4],
         ];
     }
 }
