d871995d5695a019a73c373f268a1c87ab89e8ca
robert611 <cyrkiel86@interia.pl>
2025-01-23 22:13:59 +0100

    Add missing typing in forms

--- src/Form/DriverType.php
+++ src/Form/DriverType.php
@@ -11,7 +11,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class DriverType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('name')
@@ -21,7 +21,7 @@ class DriverType extends AbstractType
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Driver::class,
