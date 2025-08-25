a674fb96a528e2bc70d15557cf89308b634b1343
robert611 <cyrkiel86@interia.pl>
2025-08-25 23:14:20 +0200

    Refactor DriverType

--- src/Form/DriverType.php
+++ src/Form/DriverType.php
@@ -1,11 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Form;
 
 use App\Entity\Driver;
 use App\Entity\Team;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,9 +18,9 @@ class DriverType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('surname')
-            ->add('car_id')
+            ->add('name', TextType::class, ['required' => true])
+            ->add('surname', TextType::class, ['required' => true])
+            ->add('car_id', NumberType::class, ['required' => true])
             ->add('team', EntityType::class, ['class' => Team::class, 'choice_label' => 'name'])
         ;
     }
