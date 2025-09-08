ba2eedece00be0859704f13625525c79a405fd51
robert611 <cyrkiel86@interia.pl>
2025-09-08 13:41:04 +0200

    Simplify getting race from database

--- src/Service/Classification/SeasonClassifications.php
+++ src/Service/Classification/SeasonClassifications.php
@@ -1,20 +1,23 @@
 <?php 
 
+declare(strict_types=1);
+
 namespace App\Service\Classification;
 
 use App\Entity\Driver;
 use App\Entity\Qualification;
-use App\Entity\Race;
 use App\Entity\Season;
 use App\Model\DriversClassification;
 use App\Repository\DriverRepository;
+use App\Repository\RaceRepository;
 use App\Service\DriverStatistics\DriverPoints;
 use Doctrine\Common\Collections\Collection;
 
 class SeasonClassifications
 {
     public function __construct(
-       private readonly DriverRepository $driverRepository,
+        private readonly DriverRepository $driverRepository,
+        private readonly RaceRepository $raceRepository,
     ) {
     }
 
@@ -50,11 +53,11 @@ class SeasonClassifications
         return $this->setDriversPositions($drivers);
     }
 
-    private function getRaceClassification(Season $season, ?int $raceId): array
+    private function getRaceClassification(Season $season, int $raceId): array
     {
         $drivers = $this->driverRepository->findAll();
 
-        $race = $this->findRace($season, $raceId);
+        $race = $this->raceRepository->findOneBy(['id' => $raceId, 'season' => $season]);
 
         /* By default, drivers have no assigned points in a database, so it has to be done here */
         foreach ($drivers as $driver) {
@@ -68,25 +71,11 @@ class SeasonClassifications
     /**
      * @return Collection<Qualification>
      */
-    private function getQualificationsClassification(Season $season, ?int $raceId): Collection
+    private function getQualificationsClassification(Season $season, int $raceId): Collection
     {
-        $race = $this->findRace($season, $raceId);
+        $race = $this->raceRepository->findOneBy(['id' => $raceId, 'season' => $season]);
 
         return $race->getQualifications();
-    }
-
-    private function findRace(Season $season, ?int $raceId): Race
-    {
-        $race = $season->getRaces()->filter(function($race) use ($raceId) {
-            return $race->getId() == $raceId;
-        })->first();
-
-        /* If user typed un proper race id, it matches the default name in twig */
-        if (!$race) {
-            $race = $season->getRaces()->first();
-        }
-
-        return $race;
     }
 
     /**
