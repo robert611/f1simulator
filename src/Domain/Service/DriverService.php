8522f5f773643d41d6a35ca2f73628254f302778
robert611 <cyrkiel86@interia.pl>
2026-01-02 14:53:14 +0100

    Improve managing database relations when updating a driver

--- src/Domain/Service/DriverService.php
+++ src/Domain/Service/DriverService.php
@@ -6,26 +6,31 @@ namespace Domain\Service;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Domain\Contract\DriverServiceFacadeInterface;
-use Domain\Entity\Team;
 use Domain\Repository\DriverRepository;
+use Domain\Repository\TeamRepository;
 
 readonly class DriverService implements DriverServiceFacadeInterface
 {
     public function __construct(
         private DriverRepository $driverRepository,
+        private TeamRepository $teamRepository,
         private EntityManagerInterface $entityManager,
     ) {
     }
 
     public function update(int $driverId, string $name, string $surname, int $teamId, int $carNumber): void
     {
+        $team = $this->teamRepository->find($teamId);
         $driver = $this->driverRepository->find($driverId);
+
+        $driver->getTeam()->removeDriver($driver);
         $driver->update(
             $name,
             $surname,
-            $this->entityManager->getReference(Team::class, $teamId),
+            $team,
             $carNumber,
         );
+        $team->addDriver($driver);
 
         $this->entityManager->flush();
     }
