5ef8a1df90a6d2f8007c9064d7af395170272905
robert611 <cyrkiel86@interia.pl>
2026-01-11 16:41:52 +0100

    Extract simulating league race logic from controller

--- src/Multiplayer/Controller/LeagueController.php
+++ src/Multiplayer/Controller/LeagueController.php
@@ -5,18 +5,12 @@ declare(strict_types=1);
 namespace Multiplayer\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
-use Domain\Contract\Configuration\RaceScoringSystem;
-use Domain\DomainFacadeInterface;
 use Multiplayer\Entity\UserSeason;
 use Multiplayer\Entity\UserSeasonPlayer;
-use Multiplayer\Entity\UserSeasonQualification;
-use Multiplayer\Entity\UserSeasonRace;
-use Multiplayer\Entity\UserSeasonRaceResult;
 use Multiplayer\Repository\UserSeasonRepository;
 use Multiplayer\Security\LeagueVoter;
 use Multiplayer\Service\DrawDriverToReplace;
 use Multiplayer\Service\GameSimulation\SimulateLeagueRace;
-use Multiplayer\Service\LeagueClassifications;
 use Shared\Controller\BaseController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -28,11 +22,9 @@ class LeagueController extends BaseController
 {
     public function __construct(
         private readonly UserSeasonRepository $userSeasonRepository,
-        private readonly SimulateLeagueRace $simulateLeagueRace,
-        private readonly LeagueClassifications $leagueClassifications,
         private readonly DrawDriverToReplace $drawDriverToReplace,
+        private readonly SimulateLeagueRace $simulateLeagueRace,
         private readonly EntityManagerInterface $entityManager,
-        private readonly DomainFacadeInterface $domainFacade,
     ) {
     }
 
@@ -92,60 +84,7 @@ class LeagueController extends BaseController
     {
         $this->denyAccessUnlessGranted(LeagueVoter::SIMULATE_RACE, $userSeason);
 
-        /** @var null|UserSeasonRace $lastRace */
-        $lastRace = $userSeason->getRaces()->last();
-        $track = $lastRace
-            ? $this->domainFacade->getNextTrack($lastRace->getTrackId())
-            : $this->domainFacade->getFirstTrack();
-
-        $connection = $this->entityManager->getConnection();
-        $connection->beginTransaction();
-
-        try {
-            /* Save race in the database */
-            $race = new UserSeasonRace();
-
-            $race->setTrackId($track->getId());
-            $race->setSeason($userSeason);
-
-            $this->entityManager->persist($race);
-            $this->entityManager->flush();
-
-            $leagueRaceResultsDTO = $this->simulateLeagueRace->simulateRaceResults($userSeason);
-
-            $qualificationsResults = $leagueRaceResultsDTO->getQualificationsResults();
-
-            foreach ($qualificationsResults->getQualificationResults() as $result) {
-                $qualification = new UserSeasonQualification();
-                $qualification->setRace($race);
-                $qualification->setPlayer($result->getUserSeasonPlayer());
-                $qualification->setPosition($result->getPosition());
-
-                $this->entityManager->persist($qualification);
-            }
-
-            $this->entityManager->flush();
-
-            $raceResults = $leagueRaceResultsDTO->getRaceResults();
-
-            /** @var UserSeasonPlayer $player */
-            foreach ($raceResults as $position => $player) {
-                $points = RaceScoringSystem::getPositionScore($position);
-
-                $raceResult = UserSeasonRaceResult::create($position, $points, $race, $player);
-                $player->addPoints($points);
-
-                $this->entityManager->persist($raceResult);
-            }
-
-            $this->entityManager->flush();
-            $connection->commit();
-        } catch (Throwable $e) {
-            $connection->rollBack();
-            throw $e;
-        }
-
-        $this->leagueClassifications->recalculatePlayersPositions($userSeason);
+        $this->simulateLeagueRace->simulateRace($userSeason);
 
         return $this->redirectToRoute('multiplayer_show_season', ['id' => $userSeason->getId()]);
     }
