ba866315e6f91ce99d5b97e727c4974619a0f8bb
robert611 <cyrkiel86@interia.pl>
2026-01-16 21:08:23 +0100

    Add test for league dashboard page

--- tests/Functional/Multiplayer/UserSeasonControllerTest.php
+++ tests/Functional/Multiplayer/UserSeasonControllerTest.php
@@ -84,7 +84,6 @@ class UserSeasonControllerTest extends WebTestCase
 
         // when
         $crawler = $this->client->request('GET', '/multiplayer');
-
         $form = $crawler->selectButton('Stwórz')->form([
             'user_season[name]' => 'Liga szybkich kierowców',
             'user_season[maxPlayers]' => '20',
@@ -100,5 +99,53 @@ class UserSeasonControllerTest extends WebTestCase
         self::assertEquals("Liga szybkich kierowców", $userSeason->getName());
         self::assertEquals(20, $userSeason->getMaxPlayers());
         self::assertEquals(1, $userSeason->getPlayers()->count());
+    }
+
+    #[Test]
+    public function league_dashboard_is_successful(): void
+    {
+        // given
+        $user = $this->fixtures->aUser();
+        $this->client->loginUser($user);
+
+        // and given
+        $track1 = $this->fixtures->aTrack('Australian Grand Prix', 'australia.png');
+        $track2 = $this->fixtures->aTrack('Bahrain Grand Prix', 'bahrain.png');
+        $track3 = $this->fixtures->aTrack('China Grand Prix', 'chinese.png');
+        $track4 = $this->fixtures->aTrack('Azerbaijan Grand Prix', 'azerbaijan.png');
+        $track5 = $this->fixtures->aTrack('Spain Grand Prix', 'spanish.png');
+        $track6 = $this->fixtures->aTrack('Monaco Grand Prix', 'monaco.png');
+
+        // and given
+        $team = $this->fixtures->aTeam();
+        $driver = $this->fixtures->aDriver("Lewis", "Hamilton", $team, 44);
+
+        // and given
+        $secret = "J783NMS092C";
+        $userSeason = $this->fixtures->aUserSeason(
+            $secret,
+            10,
+            $user,
+            "Liga szybkich kierowców",
+            false,
+            false,
+        );
+
+        // and given
+        $this->fixtures->aUserSeasonPlayer($userSeason, $user, $driver);
+
+        // and given
+        $this->fixtures->aUserSeasonRace($track1->getId(), $userSeason);
+        $this->fixtures->aUserSeasonRace($track2->getId(), $userSeason);
+        $this->fixtures->aUserSeasonRace($track3->getId(), $userSeason);
+        $this->fixtures->aUserSeasonRace($track4->getId(), $userSeason);
+        $this->fixtures->aUserSeasonRace($track5->getId(), $userSeason);
+        $this->fixtures->aUserSeasonRace($track6->getId(), $userSeason);
+
+        // when
+        $this->client->request('GET', "/multiplayer/{$userSeason->getId()}/show");
+
+        // then
+        self::assertResponseIsSuccessful();
     }
 }
