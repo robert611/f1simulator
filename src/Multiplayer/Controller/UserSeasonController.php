ea0563be5ad0747f071a306f8b77b1083084bf13
robert611 <cyrkiel86@interia.pl>
2026-01-17 18:31:54 +0100

    Refactor controllers structure in multiplayer module

--- src/Multiplayer/Controller/UserSeasonController.php
+++ src/Multiplayer/Controller/UserSeasonController.php
@@ -13,55 +13,100 @@ use Multiplayer\Entity\UserSeasonPlayer;
 use Multiplayer\Form\UserSeasonFormDTO;
 use Multiplayer\Form\UserSeasonType;
 use Multiplayer\Repository\UserSeasonRepository;
-use Multiplayer\Service\DrawDriverToReplace;
 use Multiplayer\Security\LeagueVoter;
 use Multiplayer\Service\ClassificationType;
+use Multiplayer\Service\DrawDriverToReplace;
+use Multiplayer\Service\GameSimulation\SimulateLeagueRace;
 use Multiplayer\Service\LeagueClassifications;
 use Multiplayer\Service\LeagueTeamsClassification;
 use Multiplayer\Service\SecretGenerator;
 use Shared\Controller\BaseController;
 use Shared\HashTable;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Throwable;
 
-#[Route('/multiplayer')]
+#[Route('/player-league')]
 class UserSeasonController extends BaseController
 {
     public function __construct(
-        private readonly EntityManagerInterface $entityManager,
-        private readonly LeagueTeamsClassification $leagueTeamsClassification,
-        private readonly LeagueClassifications $leagueClassification,
-        private readonly DrawDriverToReplace $drawDriverToReplace,
         private readonly UserSeasonRepository $userSeasonRepository,
+        private readonly DrawDriverToReplace $drawDriverToReplace,
+        private readonly SimulateLeagueRace $simulateLeagueRace,
         private readonly SecretGenerator $secretGenerator,
         private readonly DomainFacadeInterface $domainFacade,
+        private readonly LeagueTeamsClassification $leagueTeamsClassification,
+        private readonly LeagueClassifications $leagueClassification,
+        private readonly EntityManagerInterface $entityManager,
     ) {
     }
 
-    #[Route('', name: 'multiplayer_index', methods: ['GET', 'POST'])]
-    public function index(): Response
-    {
-        $form = $this->createForm(UserSeasonType::class, new UserSeasonFormDTO(), [
-            'action' => $this->generateUrl('multiplayer_create'),
-            'method' => 'POST',
+    #[Route('/{id}/show/{classificationType}', name: 'user_season_show', methods: ['GET'])]
+    public function show(
+        Request $request,
+        UserSeason $season,
+        ClassificationType $classificationType = ClassificationType::PLAYERS,
+    ): Response {
+        $this->denyAccessUnlessGranted(LeagueVoter::SHOW_SEASON, $season);
+
+        $player = $this->entityManager->getRepository(UserSeasonPlayer::class)->findOneBy([
+            'season' => $season,
+            'user' => $this->getUser(),
         ]);
 
-        $userLeagues = $this->userSeasonRepository->findBy(['owner' => $this->getUser()]);
-        $leagues = $this->userSeasonRepository->getUserSeasons($this->getUser()->getId());
+        $numberOfRacesInSeason = $this->domainFacade->getTracksCount();
+        $lastTrack = null;
+
+        if ($season->getRaces()->count()) {
+            $track = $this->domainFacade->getNextTrack($season->getRaces()->last()->getTrackId());
+            $lastTrack = $this->domainFacade->getTrackById($season->getRaces()->last()->getTrackId());
+        } else {
+            $track = $this->domainFacade->getFirstTrack();
+        }
+
         $tracks = $this->domainFacade->getAllTracks();
         /** @var TrackDTO[] $tracks */
         $tracks = HashTable::fromObjectArray($tracks, 'getId');
 
-        return $this->render('@multiplayer/league/index.html.twig', [
-            'form' => $form->createView(),
-            'userLeagues' => $userLeagues,
-            'leagues' => $leagues,
+        $drivers = $this->domainFacade->getAllDrivers();
+        /** @var DriverDTO[] $drivers */
+        $drivers = HashTable::fromObjectArray($drivers, 'getId');
+
+        if ($season->getRaces()->count() === 0) {
+            $classificationType = ClassificationType::PLAYERS;
+        }
+
+        $raceId = $request->query->get('race_id');
+
+        if (is_numeric($raceId)) {
+            $raceId = (int) $raceId;
+        }
+
+        $classification = $this->leagueClassification->getClassificationBasedOnType(
+            $season,
+            $classificationType,
+            $raceId,
+        );
+
+        $teamsClassification = $this->leagueTeamsClassification->getClassification($season);
+
+        return $this->render('@multiplayer/user_season/show.html.twig', [
+            'league' => $season,
+            'player' => $player,
+            'numberOfRacesInSeason' => $numberOfRacesInSeason,
+            'track' => $track,
+            'lastTrack' => $lastTrack,
             'tracks' => $tracks,
+            'drivers' => $drivers,
+            'classificationType' => $classificationType,
+            'classification' => $classification,
+            'teamsClassification' => $teamsClassification,
         ]);
     }
 
-    #[Route('/create', name: 'multiplayer_create', methods: ['POST'])]
+    #[Route('/create', name: 'user_season_create', methods: ['POST'])]
     public function create(Request $request): Response
     {
         $form = $this->createForm(UserSeasonType::class, new UserSeasonFormDTO());
@@ -98,66 +143,64 @@ class UserSeasonController extends BaseController
         return $this->redirectToRoute('multiplayer_index');
     }
 
-    #[Route('/{id}/show/{classificationType}', name: 'multiplayer_show_season', methods: ['GET'])]
-    public function showSeason(
-        Request $request,
-        UserSeason $season,
-        ClassificationType $classificationType = ClassificationType::PLAYERS,
-    ): Response {
-        $this->denyAccessUnlessGranted(LeagueVoter::SHOW_SEASON, $season);
+    #[Route('/join', name: 'user_season_join', methods: ['GET', 'POST'])]
+    public function join(Request $request): RedirectResponse
+    {
+        $secret = $request->request->get('user_season_secret');
+        $league = $this->userSeasonRepository->findOneBy(['secret' => $secret]);
 
-        $player = $this->entityManager->getRepository(UserSeasonPlayer::class)->findOneBy([
-            'season' => $season,
-            'user' => $this->getUser(),
-        ]);
+        $this->denyAccessUnlessGranted(LeagueVoter::JOIN, $league);
 
-        $numberOfRacesInSeason = $this->domainFacade->getTracksCount();
+        $driver = $this->drawDriverToReplace->getDriverToReplaceInUserLeague($league);
 
-        if ($season->getRaces()->count()) {
-            $track = $this->domainFacade->getNextTrack($season->getRaces()->last()->getTrackId());
-        } else {
-            $track = $this->domainFacade->getFirstTrack();
+        if (null === $driver) {
+            $this->addFlash('warning', 'Brakuje kierowców, w których możesz się wcielić.');
+
+            return $this->redirectToRoute('multiplayer_index');
         }
 
-        $lastTrack = $this->domainFacade->getTrackById($season->getRaces()->last()->getTrackId());
+        $player = UserSeasonPlayer::create($league, $this->getUser(), $driver->getId());
 
-        $tracks = $this->domainFacade->getAllTracks();
-        /** @var TrackDTO[] $tracks */
-        $tracks = HashTable::fromObjectArray($tracks, 'getId');
+        $this->entityManager->persist($player);
+        $this->entityManager->flush();
 
-        $drivers = $this->domainFacade->getAllDrivers();
-        /** @var DriverDTO[] $drivers */
-        $drivers = HashTable::fromObjectArray($drivers, 'getId');
+        return $this->redirectToRoute('multiplayer_index');
+    }
 
-        if ($season->getRaces()->count() === 0) {
-            $classificationType = ClassificationType::PLAYERS;
-        }
+    #[Route('/{id}/start', name: 'user_season_start', methods: ['GET'])]
+    public function start(UserSeason $season): RedirectResponse
+    {
+        $this->denyAccessUnlessGranted(LeagueVoter::START, $season);
 
-        $raceId = $request->query->get('race_id');
+        $season->start();
 
-        if (is_numeric($raceId)) {
-            $raceId = (int) $raceId;
-        }
+        $this->entityManager->flush();
 
-        $classification = $this->leagueClassification->getClassificationBasedOnType(
-            $season,
-            $classificationType,
-            $raceId,
-        );
+        return $this->redirectToRoute('user_season_show', ['id' => $season->getId()]);
+    }
 
-        $teamsClassification = $this->leagueTeamsClassification->getClassification($season);
+    #[Route('/{id}/end', name: 'user_season_end', methods: ['GET'])]
+    public function end(UserSeason $season): RedirectResponse
+    {
+        $this->denyAccessUnlessGranted(LeagueVoter::END, $season);
 
-        return $this->render('@multiplayer/league/show_league.html.twig', [
-            'league' => $season,
-            'player' => $player,
-            'numberOfRacesInSeason' => $numberOfRacesInSeason,
-            'track' => $track,
-            'lastTrack' => $lastTrack,
-            'tracks' => $tracks,
-            'drivers' => $drivers,
-            'classificationType' => $classificationType,
-            'classification' => $classification,
-            'teamsClassification' => $teamsClassification
-        ]);
+        $season->end();
+
+        $this->entityManager->flush();
+
+        return $this->redirectToRoute('user_season_show', ['id' => $season->getId()]);
+    }
+
+    /**
+     * @throws Throwable
+     */
+    #[Route('/{id}/simulate/race', name: 'user_season_simulate_race', methods: ['GET'])]
+    public function simulateRace(UserSeason $userSeason): RedirectResponse
+    {
+        $this->denyAccessUnlessGranted(LeagueVoter::SIMULATE_RACE, $userSeason);
+
+        $this->simulateLeagueRace->simulateRace($userSeason);
+
+        return $this->redirectToRoute('user_season_show', ['id' => $userSeason->getId()]);
     }
 }
