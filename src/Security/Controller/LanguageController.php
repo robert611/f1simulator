6748b30c6842c19a8f3ab463e51f88a0c178496f
robert611 <cyrkiel86@Interia.pl>
2026-01-31 21:05:09 +0100

    Add language picker to navbar

---
+++ src/Security/Controller/LanguageController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Security\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LanguageController extends AbstractController
+{
+    #[Route('/change-language/{locale}', name: 'app_change_language')]
+    public function changeLanguage(string $locale, Request $request): Response
+    {
+        $request->getSession()->set('_locale', $locale);
+
+        $referer = $request->headers->get('referer');
+
+        if ($referer) {
+            return $this->redirect($referer);
+        }
+
+        return $this->redirectToRoute('app_index');
+    }
+}
