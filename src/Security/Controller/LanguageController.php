29735bf7c513e03c7805f22af48e3667bf41c6a2
robert611 <cyrkiel86@Interia.pl>
2026-02-01 15:56:41 +0100

    Add tests for language controller

--- src/Security/Controller/LanguageController.php
+++ src/Security/Controller/LanguageController.php
@@ -7,7 +7,7 @@ namespace Security\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class LanguageController extends AbstractController
 {
