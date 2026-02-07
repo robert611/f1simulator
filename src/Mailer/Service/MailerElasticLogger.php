b4b0c2cb84bf0c7031138d2863504e93caa3ee8a
robert611 <cyrkiel86@Interia.pl>
2026-02-07 19:56:54 +0100

    Add elastic module and monolog configuration to save mailer logs to elasticsearch

---
+++ src/Mailer/Service/MailerElasticLogger.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mailer\Service;
+
+use Psr\Log\LoggerInterface;
+
+final readonly class MailerElasticLogger
+{
+    public function __construct(
+        private LoggerInterface $mailerElasticLogger,
+    ) {
+    }
+
+    public function sent(array $to, string $subject, array $context = []): void
+    {
+        $this->mailerElasticLogger->info('Mail sent', [
+            'status' => 'sent',
+            'recipients' => $to,
+            'subject' => $subject,
+            ...$context,
+        ]);
+    }
+
+    public function error(array $to, string $subject, string $error): void
+    {
+        $this->mailerElasticLogger->error('Mail error', [
+            'status' => 'error',
+            'to' => $to,
+            'recipients' => $subject,
+            'error' => $error,
+        ]);
+    }
+}
